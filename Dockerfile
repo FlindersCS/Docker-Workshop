58a07e3acb70e632ce978445abf944559af59b45
alexcrushh <alexandercrush2020@gmail.com>
2023-10-25 19:45:57 +1030

    alex commit

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ RUN apk add python3 py3-pip
 
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
-ENV ENV_1="test"
+ENV ENV_1="one"
+ENV ENV_2="two"
+ENV ENV_3="three"
+ENV ENV_4="four"
 
 COPY app/* /app
 
--- app/app.py
+++ app/app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    name = "Thomas Phillips"
-    return f"Hello, {name}! - {os.environ['ENV_1']}"
+    name = "Alex Crush"
+    fan = "crus0018"
+    return f"Hello, {name}! FAN: {fan} - {os.environ['ENV_1']}, {os.environ['ENV_2']}, {os.environ['ENV_3']}, {os.environ['ENV_4']} "
